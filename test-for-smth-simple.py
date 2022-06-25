f1d486464b596be66b8645bff66017b9839ff91e
anzakaryan <anastaszakaryan@mail.ru>
2022-06-25 23:27:05 +0300

    dima made his moves

--- test-for-smth-simple.py
+++ test-for-smth-simple.py
@@ -1,5 +1,5 @@
-def test_example_number_one():
-    assert 2>1
+def test_first_example():
+    assert 20>10
 
-def test_example_number_two():
-    assert 4>3
+def test_second_example():
+    assert 40>30
